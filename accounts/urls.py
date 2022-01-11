4d11116de9fa97b45c54ae3c37c3767ac3e9269c
ls <leonid.smirnov@yahoo.com>
2022-01-11 16:02:24 +0300

    #add users models, views, update swagger

---
+++ accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from accounts.views.auth import Signup, Login  # , Logout, UpdateToken
+from accounts.views.users import UserCredentialsView, UserAvatarView
+
+app_name = "accounts"
+
+urlpatterns = [
+
+    path('auth/signup/', Signup.as_view()),
+    path('auth/login/', Login.as_view()),
+    # path(ROOT_API_PATH + 'auth/update_token/', UpdateToken.as_view()),
+    # path(ROOT_API_PATH + 'auth/logout/', Logout.as_view()),
+
+    path('profile/user/', UserCredentialsView.as_view()),
+    path('profile/user/avatar/', UserAvatarView.as_view())
+]
